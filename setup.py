20c2ea9067018887ff8a7b25d988bd890be5b934
rory-linehan <2692329+rory-linehan@users.noreply.github.com>
2020-04-04 02:21:03 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+# Copyright 2020 Rory Linehan
+# this code is distributed under the terms of the GNU General Public License
+
+from distutils.core import setup
+
+setup(
+    name='protonvpn-controller',
+    packages=['pvpnctl.py',],
+    license='GNU GPL',
+    long_description=open('README.md').read(),
+)
